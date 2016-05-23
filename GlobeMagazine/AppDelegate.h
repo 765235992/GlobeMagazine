ace32697674927447296cf81c5749261870d1f61
huaxianwei <huaxianwei@yicai.com>
2016-05-23 11:48:53 +0800

    Initial Commit

---
+++ GlobeMagazine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GlobeMagazine
+//
+//  Created by 滑现伟 on 16/5/23.
+//  Copyright © 2016年 Demon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GlobeMagazine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GlobeMagazine
+//
+//  Created by 滑现伟 on 16/5/23.
+//  Copyright © 2016年 Demon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
